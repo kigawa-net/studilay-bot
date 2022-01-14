9f98443846d5a3a69ec9fa44f84b2e0cf7c29db7
kigawa <kigawa.8390@gmail.com>
2022-01-15 02:35:55 +0900

    update

---
+++ src/main/java/net/kigawa/discord/studilaybot/StudilayBot.java
@@ -0,0 +1,11 @@
+package net.kigawa.discord.studilaybot;
+
+public class StudilayBot {
+    private static final String TOKEN = "OTMwNjY4MjA5OTYxNTg2Njg4.Yd5OUA.yTjA7yIg44nvTNSK8JWYh1A4V_U";
+    private static final String OWNER_ID = "617576850654232608";
+    private static final String COMMAND_PREFIX = "st";
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/net/kigawa/discord/studilaybot/terminal/Terminal.java
@@ -0,0 +1,7 @@
+package net.kigawa.discord.studilaybot.terminal;
+
+import java.io.IOException;
+
+public class Terminal {
+    Exception exception = new IOException();
+}
