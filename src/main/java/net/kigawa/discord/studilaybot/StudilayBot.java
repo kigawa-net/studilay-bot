aa04a30baf4ff87c4aebbb3ef47bd5569752f7ee
kigawa <kigawa.8390@gmail.com>
2022-01-20 21:37:10 +0900

    update

--- src/main/java/net/kigawa/discord/studilaybot/StudilayBot.java
+++ src/main/java/net/kigawa/discord/studilaybot/StudilayBot.java
@@ -6,13 +6,14 @@ import net.kigawa.file.FileUtil;
 import net.kigawa.log.Formatter;
 import net.kigawa.log.Logger;
 import net.kigawa.log.TerminalHandler;
+import net.kigawa.thread.ThreadExecutors;
 
 import java.io.IOException;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 
-public class StudilayBot implements Application {
+public class StudilayBot extends Application {
     private static final String TOKEN = "OTMwNjY4MjA5OTYxNTg2Njg4.Yd5OUA.yTjA7yIg44nvTNSK8JWYh1A4V_U";
     private static final String OWNER_ID = "617576850654232608";
     private static final String COMMAND_PREFIX = "st";
@@ -33,19 +34,24 @@ public class StudilayBot implements Application {
     @Override
     public void onEnable() {
         studilayBot.addModule(Logger.getInstance());
+        studilayBot.enableAddModule(new ThreadExecutors());
         studilayBot.enableAddModule(new Terminal(J_LINE));
-
-        for (Handler handler : Logger.getLogger("").getHandlers()) {
-            if (handler instanceof ConsoleHandler) {
-                Logger.getLogger("").removeHandler(handler);
-            }
-        }
-        Logger.getLogger("").addHandler(new TerminalHandler(Terminal.terminal, new Formatter()));
 
         info("enable studilay bot");
         Terminal.terminal.addOnRead(s -> {
             if (s.equals("stop")) disable();
         });
+
+        while (isRun()) {
+            synchronized (this) {
+                try {
+                    wait(1000);
+                    info("wait");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
